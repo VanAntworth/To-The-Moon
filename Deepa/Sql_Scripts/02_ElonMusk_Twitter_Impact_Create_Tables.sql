2a0d0796e7f5133a50d3a57aaa0c3740f6f5c571
DeepaGheewala <deepaparikh18@gmail.com>
2022-07-14 16:46:17 -0500

    minor change

--- Deepa/Sql_Scripts/02_ElonMusk_Twitter_Impact_Create_Tables.sql
+++ Deepa/Sql_Scripts/02_ElonMusk_Twitter_Impact_Create_Tables.sql
@@ -47,6 +47,55 @@ CREATE TABLE "FinanceUsdData" (
 DROP TABLE IF EXISTS "PredictedValues" ;
 CREATE TABLE "PredictedValues" (
     "id" SERIAL   NOT NULL,
+    "model" VARCHAR   NOT NULL,DROP TABLE IF EXISTS "SentimentScoring" ;
+CREATE TABLE "SentimentScoring" (
+    "id" SERIAL   NOT NULL,
+    "twitterID" INT   NOT NULL,
+    "date" DATE   NOT NULL,
+    "closingPrice" FLOAT   NOT NULL,
+    "volumn" BIGINT   NOT NULL,
+    "sentimentScore" FLOAT   NOT NULL,
+    "sentiment" SMALLINT   NOT NULL,
+    CONSTRAINT "pkSentimentScoring" PRIMARY KEY (
+        "id"
+     )
+);
+
+DROP TABLE IF EXISTS "TwitterData" ;
+CREATE TABLE "TwitterData" (
+    "id" SERIAL  NOT NULL,
+    "tweetID" BIGINT   NOT NULL,
+    "financeType" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "fullText" VARCHAR   NOT NULL,
+    "replyCount" INTEGER ,
+    "likesCount" INTEGER ,
+    "retweetCount" INTEGER  ,
+    CONSTRAINT "pkTwitterData" PRIMARY KEY (
+        "id"
+     )
+);
+
+DROP TABLE IF EXISTS "FinanceUsdData" ;
+CREATE TABLE "FinanceUsdData" (
+    "id" SERIAL   NOT NULL,
+    "financeType" VARCHAR   NOT NULL,
+    "date" DATE   NOT NULL,
+    "openAt" FLOAT   NOT NULL,
+    "high" FLOAT   ,
+    "low" FLOAT   ,
+    "close" FLOAT  ,
+    "adjustedClose" FLOAT  ,
+    "volumn" BIGINT   NOT NULL,
+    CONSTRAINT "pkFinanceUsdData" PRIMARY KEY (
+        "id"
+     )
+);
+
+
+DROP TABLE IF EXISTS "PredictedValues" ;
+CREATE TABLE "PredictedValues" (
+    "id" SERIAL   NOT NULL,
     "model" VARCHAR   NOT NULL,
     "runFrom" VARCHAR   NOT NULL,
     "precision" FLOAT   NOT NULL,
@@ -63,5 +112,23 @@ CREATE TABLE "PredictedValues" (
 
 
 ALTER TABLE "SentimentScoring" ADD CONSTRAINT "fk_SentimentScoringTwitterID" FOREIGN KEY("twitterID")
-REFERENCES "twitterData" ("id");
+REFERENCES "TwitterData" ("id");
+
+
+    "runFrom" VARCHAR   NOT NULL,
+    "precision" FLOAT   NOT NULL,
+    "recall" FLOAT   NOT NULL,
+    "f1Score" FLOAT   NOT NULL,
+    "accuracy" FLOAT   NOT NULL,
+    "predicatedDogecoinValue" FLOAT   NOT NULL,
+    "date" DATE   NOT NULL,
+    "actualValue" FLOAT   NOT NULL,
+    CONSTRAINT "pkPredictedValues" PRIMARY KEY (
+        "id"
+     )
+);
+
+
+ALTER TABLE "SentimentScoring" ADD CONSTRAINT "fk_SentimentScoringTwitterID" FOREIGN KEY("twitterID")
+REFERENCES "TwitterData" ("id");
 

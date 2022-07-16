4ae12cd3c8e8fbcab7de87e7d4d148a70afbb7d0
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-07-15 22:05:54 -0500

    deltas_update1

---
+++ Tamara/config.py
@@ -0,0 +1,5 @@
+api_key = "taqH8GdiZFqRq8ec44OZBolDm"
+api_key_secret = "S3MwUuYXYlmSoBuIIv0X2moZr6Jke0lFnw4r88jPGHNB6lhhb7"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAL%2F4eQEAAAAAaFle2QjH3Ex2r9nN5%2FWmbV5uPS8%3Db7UGSN0gLbqiUJDLOyCbi37RYQHCxSeYgqv8ILZdshX6DeCgKc"
+access_token = "1544344765067661316-uJfFICkOhGrL2lNRHCdOZXHw7T8zF8"
+access_token_secret = "aRtlRggPRc56hZrdSyd9oud7bdrRv8W9ZicqLGiHbpETv"

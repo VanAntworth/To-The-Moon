09f25519eb8ed01dea0715d609d8964b8dcb7566
shreha <shrehalko@gmail.com>
2022-07-22 12:03:10 -0500

    pushing the visualization file after updating a chart

---
+++ Segment3_Delivery/Visualization/static/js/app.js
@@ -0,0 +1,11 @@
+function init() {
+    // Grab a reference to the dropdown select element
+    var dateSlected = d3.select("#dateSlected");
+  
+    // Use the list of sample names to populate the select options
+    console.log("print date")
+    console.log(dateSelected)
+}
+
+// Initialize the dashboard
+init();

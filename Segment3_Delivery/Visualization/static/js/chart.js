9c4430addde7c09312bf6d26c8508d816bd1fea7
DeepaGheewala <deepaparikh18@gmail.com>
2022-07-23 02:14:35 -0500

    Generated Chart

--- Segment3_Delivery/Visualization/static/js/chart.js
+++ Segment3_Delivery/Visualization/static/js/chart.js
@@ -50,10 +50,98 @@ var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
         setDate: today
     });
 }
+
+function buildSentimentChart(){
+    fetch('/JSON_files/financetweetsentiment.json').then(response => {
+
+        return response.json();
+      }).then(data => {
+        data = data.filter(index => index.financeType == 'doge')
+        // Work with JSON data here
+        const dateData = data.map(
+            function(index){
+                return index.date;
+                }
+            )
+        const adjustedClose = data.map(
+            function(index){
+                return index.adjustedClose;
+                }
+            )
+        const sentimentScore = data.map(
+            function(index){
+                return index.sentimentScore;
+                }
+            )
+            console.log(adjustedClose);
+            console.log(sentimentScore);
+            const ctx1 = document.getElementById('sentimentChart').getContext('2d');
+            const dataset = {
+                labels: dateData,
+                datasets: [{
+                    label: 'Adjusted Close',
+                    data: adjustedClose,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                       
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        
+                    ],
+                    tension: 0.5
+                },
+                {
+                    label: 'Sentiment Score',
+                    data: sentimentScore,
+                    backgroundColor: [
+
+                        'rgba(54, 162, 235, 0.2)',
+                      
+                    ],
+                    borderColor: [
+
+                        'rgba(54, 162, 235, 1)',
+                  
+                    ],
+                    tension: 0.5,
+                    yAxisID: 'dollar'
+                }]
+            }
+            const sentimentChart = new Chart(ctx1, {
+                type: 'line',
+                data: dataset,
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            title:{
+                                display: true,
+                                text: 'Adjusted Value'
+                            }
+                        },
+                        dollar:{
+                            beginAtZero: true,
+                            position: 'right',
+                            title:{
+                                display: true,
+                                text: 'Sentiment score'
+                            }
+                        }
+                    }
+                }
+            });
+
+       
+      }).catch(err => {
+        console.log(err);
+      });
+}
 function init()
 {
     buildDateControl();
     buildTweet7Day();
+    buildSentimentChart();
 }
 
 

0af89520429013ab93ca282bde36db96061f3d9c
DeepaGheewala <deepaparikh18@gmail.com>
2022-07-25 00:27:17 -0500

    fixed annotation issues

--- Segment3_Delivery/Visualization/static/js/SentimentChart.js
+++ Segment3_Delivery/Visualization/static/js/SentimentChart.js
@@ -1,120 +1,101 @@
 //Generate Doge Sentiment Chart
-function buildDogeSentimentChart(){
-    fetch('/JSON_files/dogeTFS.json').then(response => {
+function buildDogeSentimentChart() {
+    fetch('JSON_files/dogeTFS.json').then(response => {
 
         return response.json();
-      }).then(data => {
+    }).then(data => {
         // Work with JSON data here
         const dateData = data.map(
-            function(index){
+            function (index) {
                 return index.date;
-                }
-            )
+            }
+        )
         const adjustedClose = data.map(
-            function(index){
+            function (index) {
                 return index.adjustedClose;
-                }
-            )
+            }
+        )
         const sentimentScore = data.map(
-            function(index){
+            function (index) {
                 return index.sentimentScore;
-                }
-            )
-          //  console.log(adjustedClose);
-          //  console.log(sentimentScore);
-            const ctx1 = document.getElementById('dogeChart').getContext('2d');
-            buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore)
+            }
+        )
+        //  console.log(adjustedClose);
+        //  console.log(sentimentScore);
+        const ctx1 = document.getElementById('dogeChart').getContext('2d');
+        buildSentimentFinanceChart(ctx1, dateData, adjustedClose, sentimentScore)
 
-       const impactMarker = {
-        id:'impactMarker',
-        beforeDraw: chart => {
-            const ctx = chart.ctx;
-            console.log(chart);
-        }
-       }
-      }).catch(err => {
+     
+    }).catch(err => {
         console.log(err);
-      });
+    });
 }
 //Generate Twitter Sentiment Chart
-function buildTwitterSentimentChart(){
-    fetch('/JSON_files/twitterTFS.json').then(response => {
+function buildTwitterSentimentChart() {
+    fetch('JSON_files/twitterTFS.json').then(response => {
 
         return response.json();
-      }).then(data => {
+    }).then(data => {
         // Work with JSON data here
         const dateData = data.map(
-            function(index){
+            function (index) {
                 return index.date;
-                }
-            )
+            }
+        )
         const adjustedClose = data.map(
-            function(index){
+            function (index) {
                 return index.adjustedClose;
-                }
-            )
+            }
+        )
         const sentimentScore = data.map(
-            function(index){
+            function (index) {
                 return index.sentimentScore;
-                }
-            )
-          //  console.log(adjustedClose);
-          //  console.log(sentimentScore);
-            const ctx1 = document.getElementById('twitterChart').getContext('2d');
-            buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore)
+            }
+        )
+        //  console.log(adjustedClose);
+        //  console.log(sentimentScore);
+        const ctx1 = document.getElementById('twitterChart').getContext('2d');
+        buildSentimentFinanceChart(ctx1, dateData, adjustedClose, sentimentScore)
 
-       const impactMarker = {
-        id:'impactMarker',
-        beforeDraw: chart => {
-            const ctx = chart.ctx;
-            console.log(chart);
-        }
-       }
-      }).catch(err => {
+    }).catch(err => {
         console.log(err);
-      });
+    });
 }
 //Generate Tesla Sentiment Chart
-function buildTeslaSentimentChart(){
-    fetch('/JSON_files/teslaTFS.json').then(response => {
+function buildTeslaSentimentChart() {
+    fetch('JSON_files/teslaTFS.json').then(response => {
 
         return response.json();
-      }).then(data => {
+    }).then(data => {
+        data = data.filter(index => new Date(index.date) > new Date('2020/01/01'))
         // Work with JSON data here
         const dateData = data.map(
-            function(index){
+            function (index) {
                 return index.date;
-                }
-            )
+            }
+        )
         const adjustedClose = data.map(
-            function(index){
+            function (index) {
                 return index.adjustedClose;
-                }
-            )
+            }
+        )
         const sentimentScore = data.map(
-            function(index){
+            function (index) {
                 return index.sentimentScore;
-                }
-            )
-          //  console.log(adjustedClose);
-          //  console.log(sentimentScore);
-            const ctx1 = document.getElementById('teslaChart').getContext('2d');
-            buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore)
+            }
+        )
+        //  console.log(adjustedClose);
+        //  console.log(sentimentScore);
+        const ctx1 = document.getElementById('teslaChart').getContext('2d');
+        buildSentimentFinanceChart(ctx1, dateData, adjustedClose, sentimentScore)
 
-       const impactMarker = {
-        id:'impactMarker',
-        beforeDraw: chart => {
-            const ctx = chart.ctx;
-            console.log(chart);
-        }
-       }
-      }).catch(err => {
+    }).catch(err => {
         console.log(err);
-      });
+    });
 }
 // Common function for plotting chart
-function buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore){
-    console.log("test")
+function buildSentimentFinanceChart(ctx1, dateData, adjustedClose, sentimentScore) {
+   
     const dataset = {
         labels: dateData,
         datasets: [{
@@ -122,17 +103,18 @@ function buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore){
             data: adjustedClose,
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
-               
+
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
-                
+
             ],
             tension: 0.5,
-            pointHoverBorderColor:'white',
-            pointHoverBorderColor:'rgba(255,99,132,1)',
-            pointHoverBorderWidth:3,
-            pointHoverRadius:10
+            width:0.5,
+            pointHoverBorderColor: 'white',
+            pointHoverBorderColor: 'rgba(255,99,132,1)',
+            pointHoverBorderWidth: 3,
+            pointHoverRadius: 10
         },
         {
             label: 'Sentiment Score',
@@ -140,21 +122,22 @@ function buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore){
             backgroundColor: [
 
                 'rgba(54, 162, 235, 0.2)',
-              
+
             ],
             borderColor: [
 
                 'rgba(54, 162, 235, 1)',
-          
+
             ],
             tension: 0.5,
-            pointHoverBorderColor:'white',
-            pointHoverBorderColor:'rgba(54, 162, 235, 1)',
-            pointHoverBorderWidth:3,
-            pointHoverRadius:10,
+            pointHoverBorderColor: 'white',
+            pointHoverBorderColor: 'rgba(54, 162, 235, 1)',
+            pointHoverBorderWidth: 3,
+            pointHoverRadius: 10,
             yAxisID: 'dollar'
         }]
     }
+
     const chart = new Chart(ctx1, {
         type: 'line',
         data: dataset,
@@ -162,28 +145,48 @@ function buildSentimentFinanceChart(ctx1,dateData,adjustedClose,sentimentScore){
             scales: {
                 y: {
                     beginAtZero: true,
-                    title:{
+                    title: {
                         display: true,
                         text: 'Adjusted Value'
                     }
                 },
-                dollar:{
+                dollar: {
                     beginAtZero: true,
                     position: 'right',
-                    title:{
+                    title: {
                         display: true,
                         text: 'Sentiment score'
                     }
                 }
+            },
+            plugins: {
+                autocolors:false,
+                annotation:{
+                    annotations:[{
+                        type: 'line', 
+                        scaleID: 'x', 
+                        value: 35,
+                        borderWidth: 2, 
+                        borderColor: 'black' 
+                    },
+                    {
+                        type: 'line', 
+                        scaleID: 'x', 
+                        value: 37,
+                        borderWidth: 2, 
+                        borderColor: 'magenta' 
+                    }
+                ]
+                }
             }
         }
-       
+
     });
+
+    console.log(chart.options.plugins.annotation.annotations.line1.value);
 }
 
-function init()
-{
-    console.log("new file");
+function init() {
     buildDogeSentimentChart();
     buildTeslaSentimentChart();
     buildTwitterSentimentChart();

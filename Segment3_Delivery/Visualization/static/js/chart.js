724a9d0bb0b22ea9db6d1e5eccd10170e7afc946
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-07-25 00:05:52 -0500

    data filtering for 7 days completed

--- Segment3_Delivery/Visualization/static/js/chart.js
+++ Segment3_Delivery/Visualization/static/js/chart.js
@@ -64,13 +64,86 @@ init();
 
 /*Events*/
 function optionChanged(value){
-    console.log(value)
+    
+    dateLogged = value
+    console.log(dateLogged)
+
     const label = document.getElementById("financeType");
     financeType = label.value
 
     console.log(financeType)
+
+    /* Data Fetching */
+
+    fetch('/JSON_files/financeData7Days.json').then(response1 => {
+        return response1.json();
+        }).then(data1 => {
     
+        data1 = data1[financeType]
+        
+        // console.log(data1)
+
+        Object.entries(data1).forEach(([key, value]) => {
+            if(value.tweetDate == dateLogged){
+                // console.log(key, value);
+                financeDates = value.financeDates
+                volumes = value.volumes
+                adjustedClosed = value.adjustedClosed
+            }
+         });
+        
+        console.log(financeDates)
+        console.log(volumes)
+        console.log(adjustedClosed)
+    });
+
+    fetch('/JSON_files/financedeltapercent.json').then(response2 => {
+        return response2.json();
+        }).then(data2 => {
+        
+        data2 = data2.filter(index => index.financeType == financeType )
+        
+        // console.log(data2)
+
+        for(var i=0; i < data2.length; i++){ 
+            if(data2[i].date== dateLogged) {
+                // console.log(data2[i])
+                
+                percentPrice0 = data2[i].percentPrice_0
+                percentPrice1 = data2[i].percentPrice_1
+                percentPrice2 = data2[i].percentPrice_2
+                percentPrice3 = data2[i].percentPrice_3
+                percentPrice4 = data2[i].percentPrice_4
+
+                percentVol0 = data2[i].percentVol_0
+                percentVol1 = data2[i].percentVol_1
+                percentVol2 = data2[i].percentVol_2
+                percentVol3 = data2[i].percentVol_3
+                percentVol4 = data2[i].percentVol_4
+            }
+        }      
+
+        // console.log(percentPrice0)
+        // console.log(percentVol0)
+        
+    })
+
+    fetch('/JSON_files/tweetsperDate.json').then(response3 => {
+        return response3.json();
+        }).then(data3 => {
+    
+        data3 = data3[financeType]
+
+        filteredDate3 = data3.filter(index => index.date==dateLogged)
+        
+        tweetIDsperDate = filteredDate3[0].tweetIDs
+
+        // console.log(tweetIDsperDate)
+    })
+   
+
 }
+
 
 function selectorChanged(value1){
     financeType1 = value1
@@ -80,22 +153,58 @@ function selectorChanged(value1){
     dateLogged1 = label.value
     console.log(dateLogged1)
 
-    // fetch('/JSON_files/financeData7Days.json').then(response1 => {
-    //     return response1.json();
-    //     }).then(data1 => {
+    /* Data Fetching */
+    fetch('/JSON_files/financeData7Days.json').then(response1 => {
+        return response1.json();
+        }).then(data1 => {
     
-    //     data1 = data1[financeType1]
-    //     filteredbyDate = data1.filter(index => index.tweetDate == dateLogged1)
-    //     console.log(filteredbyDate)
-    // })
+        data1 = data1[financeType1]
+        
+        // console.log(data1)
+
+        Object.entries(data1).forEach(([key, value]) => {
+            if(value.tweetDate == dateLogged1){
+                // console.log(key, value);
+                financeDates = value.financeDates
+                volumes = value.volumes
+                adjustedClosed = value.adjustedClosed
+            }
+         });
+        
+        console.log(financeDates)
+        console.log(volumes)
+        console.log(adjustedClosed)
+    });
 
     fetch('/JSON_files/financedeltapercent.json').then(response2 => {
         return response2.json();
         }).then(data2 => {
         
         data2 = data2.filter(index => index.financeType == financeType1 )
-        // filteredDate2 = data2.filter(index => index.tweetDate ==dateLogged1)
-        console.log(data2)
+        
+        // console.log(data2)
+
+        for(var i=0; i < data2.length; i++){ 
+            if(data2[i].date== dateLogged1) {
+                // console.log(data2[i])
+                
+                percentPrice0 = data2[i].percentPrice_0
+                percentPrice1 = data2[i].percentPrice_1
+                percentPrice2 = data2[i].percentPrice_2
+                percentPrice3 = data2[i].percentPrice_3
+                percentPrice4 = data2[i].percentPrice_4
+
+                percentVol0 = data2[i].percentVol_0
+                percentVol1 = data2[i].percentVol_1
+                percentVol2 = data2[i].percentVol_2
+                percentVol3 = data2[i].percentVol_3
+                percentVol4 = data2[i].percentVol_4
+            }
+        }      
+
+        // console.log(percentPrice0)
+        // console.log(percentVol0)
+        
     })
 
     fetch('/JSON_files/tweetsperDate.json').then(response3 => {
@@ -104,11 +213,11 @@ function selectorChanged(value1){
     
         data3 = data3[financeType1]
 
-        filteredDate3 = data3.filter(index => index.date==dateLogged1)
+        filteredDate3 = data3.filter(index => index.date==dateLogged1 )
         
-        // tweetIDsperDate = filteredDate3.tweetIDs
+        tweetIDsperDate = filteredDate3[0].tweetIDs
 
-        console.log(filteredDate3)
+        // console.log(tweetIDsperDate)
     })
 
 

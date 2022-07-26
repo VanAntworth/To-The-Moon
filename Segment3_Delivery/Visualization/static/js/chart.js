0aa7f81e3620a30b0be948e1d94371a6c882c696
tambam <98443002+tamiespinosa@users.noreply.github.com>
2022-07-26 11:41:28 -0500

    Adds 7 day graph when the right date is selected

--- Segment3_Delivery/Visualization/static/js/chart.js
+++ Segment3_Delivery/Visualization/static/js/chart.js
@@ -1,40 +1,69 @@
-function buildTweet7Day(){
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
+function buildTweet7Day(ctx1,financeDates,volume,adjustedClosed){
+    console.log("test")
+    const dataset = {
+        labels: financeDates,
+        datasets: [{
+            label: 'Adjusted Close',
+            data: adjustedClosed,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+               
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                
+            ],
+            tension: 0.5,
+            pointHoverBorderColor:'white',
+            pointHoverBorderColor:'rgba(255,99,132,1)',
+            pointHoverBorderWidth:3,
+            pointHoverRadius:10
         },
+        {
+            label: 'Trading Stock Volume',
+            data: volume,
+            backgroundColor: [
+
+                'rgba(54, 162, 235, 0.2)',
+              
+            ],
+            borderColor: [
+
+                'rgba(54, 162, 235, 1)',
+          
+            ],
+            tension: 0.5,
+            pointHoverBorderColor:'white',
+            pointHoverBorderColor:'rgba(54, 162, 235, 1)',
+            pointHoverBorderWidth:3,
+            pointHoverRadius:10,
+            yAxisID: 'dollar'
+        }]
+    }
+    const chart = new Chart(ctx1, {
+        type: 'line',
+        data: dataset,
         options: {
             scales: {
                 y: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    title:{
+                        display: true,
+                        text: 'Adjusted Value'
+                    }
+                },
+                dollar:{
+                    beginAtZero: true,
+                    position: 'right',
+                    title:{
+                        display: true,
+                        text: 'Trading Stock Volume'
+                    }
                 }
             }
         }
-        });
-
+       
+    });
 }
 
 function buildDateControl(){
@@ -55,7 +84,7 @@ console.log("test")
 function init()
 {
     buildDateControl();
-    buildTweet7Day();
+    // buildTweet7Day();
 
 }
 
@@ -63,26 +92,9 @@ function init()
 init();
 
 /*Events*/
+function optionChanged(value){/*Events*/
 function optionChanged(value){
-    const display = document.getElementById("displayDateSelected");
-    const ele1 = document.getElementById("tweeter-timeline");
-    const ele = document.getElementById("selected-tweet");
-    if (ele.style.display == "block") {
-        ele.setAttribute("tweetID","1538406040374595584");
-        ele.style.display = "none";
-
-        ele1.style.display = "block";
-        display.innerHTML = "show";
-    }
-    else {
-        ele.setAttribute("tweetID", "1544743525585141760");
-        ele.style.display = "block";
-        ele1.style.display = "none";
-        display.innerHTML = "hide";
-    }
-
-    console.log(ele.style.display);
-    console.log(ele1.style.display);
+    
     dateLogged = value
     console.log(dateLogged)
 
@@ -93,72 +105,79 @@ function optionChanged(value){
 
     /* Data Fetching */
 
-    fetch('JSON_files/financeData7Days.json').then(response1 => {
-        return response1.json();
-        }).then(data1 => {
-    
-        data1 = data1[financeType]
+    try {
+
+        fetch('/JSON_files/financeData7Days.json').then(response1 => {
+            return response1.json();
+            }).then(data1_oc => {
         
-        // console.log(data1)
+            data1_oc = data1_oc[financeType]
+            
+            // console.log(data1)
 
-        Object.entries(data1).forEach(([key, value]) => {
-            if(value.tweetDate == dateLogged){
-                // console.log(key, value);
-                financeDates = value.financeDates
-                volumes = value.volumes
-                adjustedClosed = value.adjustedClosed
-            }
-         });
+            Object.entries(data1_oc).forEach(([key, value]) => {
+                if(value.tweetDate == dateLogged){
+                    // console.log(key, value);
+                    financeDates_oc = value.financeDates
+                    volumes_oc = value.volumes
+                    adjustedClosed_oc = value.adjustedClosed
+                }
+            });
+            
+            console.log(financeDates_oc)
+            console.log(volumes_oc)
+            console.log(adjustedClosed_oc)
+
+            ctx = document.getElementById('myChart').getContext('2d');
+
+            buildTweet7Day(ctx,financeDates_oc,volumes_oc,adjustedClosed_oc)
+        });
+
+        fetch('/JSON_files/financedeltapercent.json').then(response2 => {
+            return response2.json();
+            }).then(data2_oc => {
+            
+            data2_oc = data2_oc.filter(index => index.financeType == financeType )
+            
+            // console.log(data2)
+
+            for(var i=0; i < data2_oc.length; i++){ 
+                if(data2_oc[i].date== dateLogged) {
+                    // console.log(data2[i])
+                    
+                    percentPrice0_oc = data2[i].percentPrice_0
+                    percentPrice1_oc = data2[i].percentPrice_1
+                    percentPrice2_oc = data2[i].percentPrice_2
+                    percentPrice3_oc = data2[i].percentPrice_3
+                    percentPrice4_oc = data2[i].percentPrice_4
+
+                    percentVol0_oc = data2[i].percentVol_0
+                    percentVol1_oc = data2[i].percentVol_1
+                    percentVol2_oc = data2[i].percentVol_2
+                    percentVol3_oc = data2[i].percentVol_3
+                    percentVol4_oc = data2[i].percentVol_4
+                }
+            }      
+
+            console.log(percentPrice0_oc)
+            console.log(percentVol0_oc)
+            
+        })
+
+        fetch('/JSON_files/tweetsperDate.json').then(response3 => {
+            return response3.json();
+            }).then(data3_oc => {
         
-        console.log(financeDates)
-        console.log(volumes)
-        console.log(adjustedClosed)
-    });
+            data3_oc = data3_oc[financeType]
 
-    fetch('JSON_files/financedeltapercent.json').then(response2 => {
-        return response2.json();
-        }).then(data2 => {
-        
-        data2 = data2.filter(index => index.financeType == financeType )
-        
-        // console.log(data2)
+            filteredDate3_oc = data3_oc.filter(index => index.date==dateLogged)
+            
+            tweetIDsperDate_oc = filteredDate3_oc[0].tweetIDs
 
-        for(var i=0; i < data2.length; i++){ 
-            if(data2[i].date== dateLogged) {
-                // console.log(data2[i])
-                
-                percentPrice0 = data2[i].percentPrice_0
-                percentPrice1 = data2[i].percentPrice_1
-                percentPrice2 = data2[i].percentPrice_2
-                percentPrice3 = data2[i].percentPrice_3
-                percentPrice4 = data2[i].percentPrice_4
+            console.log(tweetIDsperDate_oc)
+    }) }
 
-                percentVol0 = data2[i].percentVol_0
-                percentVol1 = data2[i].percentVol_1
-                percentVol2 = data2[i].percentVol_2
-                percentVol3 = data2[i].percentVol_3
-                percentVol4 = data2[i].percentVol_4
-            }
-        }      
-
-        // console.log(percentPrice0)
-        // console.log(percentVol0)
-        
-    })
-
-    fetch('JSON_files/tweetsperDate.json').then(response3 => {
-        return response3.json();
-        }).then(data3 => {
-    
-        data3 = data3[financeType]
-
-        filteredDate3 = data3.filter(index => index.date==dateLogged)
-        
-        tweetIDsperDate = filteredDate3[0].tweetIDs
-
-        // console.log(tweetIDsperDate)
-    })
-   
+    catch(err) {console.log('No record exhists')}
 
 }
 
@@ -172,71 +191,79 @@ function selectorChanged(value1){
     console.log(dateLogged1)
 
     /* Data Fetching */
-    fetch('JSON_files/financeData7Days.json').then(response1 => {
-        return response1.json();
-        }).then(data1 => {
-    
-        data1 = data1[financeType1]
+
+    try {
+
+        fetch('/JSON_files/financeData7Days.json').then(response1 => {
+            return response1.json();
+            }).then(data1 => {
         
-        // console.log(data1)
+            data1 = data1[financeType1]
+            
+            // console.log(data1)
 
-        Object.entries(data1).forEach(([key, value]) => {
-            if(value.tweetDate == dateLogged1){
-                // console.log(key, value);
-                financeDates = value.financeDates
-                volumes = value.volumes
-                adjustedClosed = value.adjustedClosed
-            }
-         });
+            Object.entries(data1).forEach(([key, value]) => {
+                if(value.tweetDate == dateLogged1){
+                    // console.log(key, value);
+                    financeDates = value.financeDates
+                    volumes = value.volumes
+                    adjustedClosed = value.adjustedClosed
+                }
+            });
+            
+            console.log(financeDates)
+            console.log(volumes)
+            console.log(adjustedClosed)
+
+            ctx = document.getElementById('myChart').getContext('2d');
+
+            buildTweet7Day(ctx,financeDates,volume,adjustedClosed)
+        });
+
+        fetch('/JSON_files/financedeltapercent.json').then(response2 => {
+            return response2.json();
+            }).then(data2 => {
+            
+            data2 = data2.filter(index => index.financeType == financeType1 )
+            
+            // console.log(data2)
+
+            for(var i=0; i < data2.length; i++){ 
+                if(data2[i].date== dateLogged1) {
+                    // console.log(data2[i])
+                    
+                    percentPrice0 = data2[i].percentPrice_0
+                    percentPrice1 = data2[i].percentPrice_1
+                    percentPrice2 = data2[i].percentPrice_2
+                    percentPrice3 = data2[i].percentPrice_3
+                    percentPrice4 = data2[i].percentPrice_4
+
+                    percentVol0 = data2[i].percentVol_0
+                    percentVol1 = data2[i].percentVol_1
+                    percentVol2 = data2[i].percentVol_2
+                    percentVol3 = data2[i].percentVol_3
+                    percentVol4 = data2[i].percentVol_4
+                }
+            }      
+
+            console.log(percentPrice0)
+            console.log(percentVol0)
+            
+        })
+
+        fetch('/JSON_files/tweetsperDate.json').then(response3 => {
+            return response3.json();
+            }).then(data3 => {
         
-        console.log(financeDates)
-        console.log(volumes)
-        console.log(adjustedClosed)
-    });
+            data3 = data3[financeType1]
 
-    fetch('JSON_files/financedeltapercent.json').then(response2 => {
-        return response2.json();
-        }).then(data2 => {
-        
-        data2 = data2.filter(index => index.financeType == financeType1 )
-        
-        // console.log(data2)
+            filteredDate3 = data3.filter(index => index.date==dateLogged1 )
+            
+            tweetIDsperDate = filteredDate3[0].tweetIDs
 
-        for(var i=0; i < data2.length; i++){ 
-            if(data2[i].date== dateLogged1) {
-                // console.log(data2[i])
-                
-                percentPrice0 = data2[i].percentPrice_0
-                percentPrice1 = data2[i].percentPrice_1
-                percentPrice2 = data2[i].percentPrice_2
-                percentPrice3 = data2[i].percentPrice_3
-                percentPrice4 = data2[i].percentPrice_4
+            console.log(tweetIDsperDate)
+    }) }
 
-                percentVol0 = data2[i].percentVol_0
-                percentVol1 = data2[i].percentVol_1
-                percentVol2 = data2[i].percentVol_2
-                percentVol3 = data2[i].percentVol_3
-                percentVol4 = data2[i].percentVol_4
-            }
-        }      
-
-        // console.log(percentPrice0)
-        // console.log(percentVol0)
-        
-    })
-
-    fetch('JSON_files/tweetsperDate.json').then(response3 => {
-        return response3.json();
-        }).then(data3 => {
-    
-        data3 = data3[financeType1]
-
-        filteredDate3 = data3.filter(index => index.date==dateLogged1 )
-        
-        tweetIDsperDate = filteredDate3[0].tweetIDs
-
-        // console.log(tweetIDsperDate)
-    })
-
+    catch(err) {console.log('No record exhists')}
 
 }
